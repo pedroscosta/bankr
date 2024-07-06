63141e2d2b0f81f01c89e3597fc977c406916b5a
pedroscosta <pedro.costa@buson.com.br>
2024-07-06 15:03:17 -0300

    fix: not redirecting when not logged

--- apps/web/src/guards/auth.tsx
+++ apps/web/src/guards/auth.tsx
@@ -1,13 +1,11 @@
 import { getCookie } from "@/lib/utils";
 import { jwtDecode } from "jwt-decode";
 import { ReactNode } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 type Props = { children: ReactNode; redirect: string; reverse?: boolean };
 
 const AuthGuard = ({ children, redirect, reverse = false }: Props) => {
-  const navigate = useNavigate();
-
   // If we don't have a valid token, redirect
   // If reverse, render the children
 
@@ -29,7 +27,7 @@ const AuthGuard = ({ children, redirect, reverse = false }: Props) => {
   console.log(isAuthenticated);
 
   if ((!reverse && !isAuthenticated) || (reverse && isAuthenticated)) {
-    navigate(redirect, { replace: true });
+    return <Navigate to={redirect} />;
   }
 
   return <>{children}</>;
